78c7a0cc5841803641a87e6555d92b388bf054db
Kate Dunne <kate.dunne@universalfieldrobots.com.au>
2023-04-14 15:37:10 +1000

    Added BTCPP_format="4" tag to XML generated by
    writeTreeNodesModelXML

--- src/xml_parsing.cpp
+++ src/xml_parsing.cpp
@@ -1009,6 +1009,7 @@ std::string writeTreeNodesModelXML(const BehaviorTreeFactory& factory,
   XMLDocument doc;
 
   XMLElement* rootXML = doc.NewElement("root");
+  rootXML->SetAttribute("BTCPP_format", 4);
   doc.InsertFirstChild(rootXML);
 
   XMLElement* model_root = doc.NewElement("TreeNodesModel");
